10873572ce9ce36321ed16761d1fd0afbe4a35b0
Anastasiia <anastasiianikolaienko2004@gmail.com>
2025-04-11 15:48:17 +0200

    1

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,12 +2,12 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const galleryContainer = document.querySelector(".gallery");
+let lightbox = null;
+const endMessageContainer = document.querySelector(".end-message");
+const loadMoreBtn = document.querySelector(".load-more"); 
 
-let lightbox; 
-
-export function createGallery(images) {
-  const markup = images
-    .map(
+export function createGallery(images, isNewGallery) {
+  const markup = images.map(
       ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
       <a href="${largeImageURL}" class="gallery-item">
         <img src="${webformatURL}" alt="${tags}" />
@@ -18,36 +18,49 @@ export function createGallery(images) {
           <p>Downloads ${downloads}</p>
         </div>
       </a>`
-    )
-    .join("");
+    ).join("");
 
-  galleryContainer.innerHTML = markup;
-
-
-  if (!lightbox) {
-    lightbox = new SimpleLightbox(".gallery a", {
-      captionsData: "alt",
-      captionDelay: 250,
-    });
-  } else {
-    lightbox.refresh(); 
-  }
-}
-
-export function clearGallery() {
-  galleryContainer.innerHTML = "";
+    if (isNewGallery) {
+      galleryContainer.innerHTML = markup;
+    } else {
+      galleryContainer.insertAdjacentHTML("beforeend", markup);
+    }
   
-  if (lightbox) {
-    lightbox.destroy();
+    if (!lightbox) {
+      lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+      });
+    } else {
+      lightbox.refresh();
+    }
   }
   
-  lightbox = null; 
-}
-
-export function showLoader() {
-  document.querySelector(".loader").classList.add("visible");
-}
-
-export function hideLoader() {
-  document.querySelector(".loader").classList.remove("visible");
-}
+  export function clearGallery() {
+    galleryContainer.innerHTML = "";
+    if (lightbox) {
+      lightbox.destroy();
+      lightbox = null;
+    }
+  }
+  
+  export function showLoader() {
+    document.querySelector(".loader").classList.add("visible");
+  }
+  
+  export function hideLoader() {
+    document.querySelector(".loader").classList.remove("visible");
+  }
+  
+  export function showEndMessage() {
+    endMessageContainer.classList.remove("hidden");
+    endMessageContainer.textContent = "We're sorry, but you've reached the end of search results.";
+  }
+  
+  export function showLoadMoreButton() {
+    loadMoreBtn.classList.remove("hidden");
+  }
+  
+  export function hideLoadMoreButton() {
+    loadMoreBtn.classList.add("hidden");
+  }
